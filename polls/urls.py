113f76c190dbce273d7f2a12bafac8c729b702d4
greatWeber <my_hero_666@sina.com>
2018-03-16 23:47:27 +0800

    添加注册

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
 	url(r'/',views.index,name='index'),
 	url(r'login',views.mlogin,name='login'),
+	url(r'register',views.register,name='register'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse, JsonResponse
 
 from django.contrib.auth import login, logout, authenticate
 
+from django.contrib.auth.models import User
+
 def index(request):
 	return HttpResponse('hello welcome to Django!')
 
@@ -20,7 +22,29 @@ def mlogin(request):
             data = {'status':1,'info':'登陆成功','url':'/polls/index'}
         else:
 
-            data = {'status':0,'info':'登陆失败','url':'/polls/mlogin'}
+            data = {'status':0,'info':'登陆失败','url':'/polls/login'}
         return JsonResponse(data)
     else:
         return render(request,'polls/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        psd = request.POST.get('password','')
+        repsd = request.POST.get('repassword','')
+        if psd != repsd:
+            data = {'status':-1,'info':'确认密码不一致','url':'/polls/register'}
+        elif User.objects.filter(username=name):
+            data = {'status':-2,'info':'已存在用户','url':'/polls/register'}
+        else:
+            user = User.objects.create_user(username=name,password=psd,email='')
+            user.save();
+            if user.id:
+                data = {'status':1,'info':'注册成功','url':'/polls/login'}
+            else:
+                data = {'status':0,'info':'注册失败','url':'/polls/register'}
+
+        return JsonResponse(data)
+    else:
+        return render(request,'polls/register.html')
