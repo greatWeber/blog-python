f0d1586e23fa540d3c0e5cf263ba58acf08eca2d
greatWeber <my_hero_666@sina.com>
2018-03-16 22:09:37 +0800

    添加登陆

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'index',views.index,name='index'),
+	url(r'/',views.index,name='index'),
+	url(r'login',views.mlogin,name='login'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from django.contrib.auth import login, logout, authenticate
 
 def index(request):
 	return HttpResponse('hello welcome to Django!')
 
+
+def mlogin(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        psd = request.POST.get('password','')
+        user = authenticate(username=name,password=psd)
+        if user is not None:
+            login(request, user)
+            data = {'status':1,'info':'登陆成功','url':'/polls/index'}
+        else:
+
+            data = {'status':0,'info':'登陆失败','url':'/polls/mlogin'}
+        return JsonResponse(data)
+    else:
+        return render(request,'polls/login.html')
